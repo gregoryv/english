08ae2caf6bdead418ef9bb4159189eb6d8d90201
Gregory Vincic <g@7de.se>
2020-12-30 10:11:57 +0100

    Cmd eng takes verb, noun and adj as random values

--- cmd/eng/main.go
+++ cmd/eng/main.go
@@ -8,14 +8,25 @@ import (
 )
 
 func main() {
-	r := flag.String("r", "", "random [word]")
+	r := flag.String("r", "", "random: word, verb, noun or adjective(adj)")
 	flag.Parse()
 
 	lang := english.NewLanguage()
 
-	switch *r {
-	case "word":
-		fmt.Println(english.RandWord(lang.Words()))
+	switch {
+	case *r != "":
+		var words func() []english.Word
+		switch *r {
+		case "word":
+			words = lang.Words
+		case "verb":
+			words = lang.Verbs
+		case "noun":
+			words = lang.Nouns
+		case "adjective", "adj":
+			words = lang.Adjectives
+		}
+		fmt.Println(english.RandWord(words()))
 	default:
 		fmt.Println(lang)
 	}
