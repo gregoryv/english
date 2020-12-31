ff6a4d51193eb32e286ae22a95526d45b5eb67c1
Gregory Vincic <g@7de.se>
2020-12-31 16:48:26 +0100

    Refactor adverbs

--- adverbs.go
+++ adverbs.go
@@ -7,100 +7,29 @@ import "strings"
 func Adverbs() []string {
 	all := make([]string, 0)
 	for _, words := range []string{
-		adverbsHow,
-		adverbsWhen,
-		adverbsWhere,
-		adverbsWhatExtent,
+		HowAdverbWords,
+		WhenAdverbWords,
+		WhereAdverbWords,
+		WhatExtentAdverbWords,
 	} {
-		all = append(all, strings.Split(strings.TrimSpace(words), "\n")...)
+		all = append(all, strings.Fields(words)...)
 	}
 	return all
 }
 
-const adverbsHow = `
-absentmindedly
-adoringly
-awkwardly
-beatifully
-briskly
-brutally
-carefully
-cheerfully
-competitively
-eagerly
-effortlessly
-extravagantly
-girlishly
-gracefully
-grimly
-happily
-halfheartedly
-hungrily
-lazily
-lifelessly
-loyally
-quickly
-quitely
-quizzically
-really
-recklessly
-remorsefully
-ruthlessly
-savagely
-sloppily
-so
-stylishly
-unabashedly
-unevenly
-urgently
-well
-wishfully
-worriedly
-`
+const HowAdverbWords = `absentmindedly adoringly awkwardly beatifully
+briskly brutally carefully cheerfully competitively eagerly
+effortlessly extravagantly girlishly gracefully grimly happily
+halfheartedly hungrily lazily lifelessly loyally quickly quitely
+quizzically really recklessly remorsefully ruthlessly savagely
+sloppily so stylishly unabashedly unevenly urgently well wishfully
+worriedly`
 
-const adverbsWhen = `
-after
-afterwards
-annually
-before
-daily
-never
-now
-soon
-still
-then
-today
-tomorrow
-weekly
-when
-yesterday
-`
+const WhenAdverbWords = `after afterwards annually before daily never
+now soon still then today tomorrow weekly when yesterday`
 
-const adverbsWhere = `
-abroad
-anywhere
-away
-down
-everywhere
-here
-home
-in
-inside
-out
-outside
-somewhere
-there
-underground
-upstairs
-`
+const WhereAdverbWords = `abroad anywhere away down everywhere here
+home in inside out outside somewhere there underground upstairs`
 
-const adverbsWhatExtent = `
-extremely
-not
-quite
-rather
-really
-terribly
-too
-very
-`
+const WhatExtentAdverbWords = `extremely not quite rather really
+terribly too very`
