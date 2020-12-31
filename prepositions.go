6b9e07e63d2f3f9b3636581d9e18d009122b5ac0
Gregory Vincic <g@7de.se>
2020-12-31 16:59:56 +0100

    Refactor prepositions

--- prepositions.go
+++ prepositions.go
@@ -3,60 +3,12 @@ package english
 import "strings"
 
 func Prepositions() []string {
-	return strings.Split(SinglePrepositionWords, "\n")
+	return strings.Fields(SinglePrepositionWords)
 }
 
-const SinglePrepositionWords = `about
-beside
-near
-to
-above
-between
-of
-towards
-across
-beyond
-off
-under
-after
-by
-on
-underneath
-against
-despite
-onto
-unlike
-along
-down
-opposite
-until
-among
-during
-out
-up
-around
-except
-outside
-upon
-as
-for
-over
-via
-at
-from
-past
-with
-before
-in
-round
-within
-behind
-inside
-since
-without
-below
-into
-than
-beneath
-like
+const SinglePrepositionWords = `about beside near to above between of
+towards across beyond off under after by on underneath against despite
+onto unlike along down opposite until among during out up around
+except outside upon as for over via at from past with before in round
+within behind inside since without below into than beneath like
 through`
